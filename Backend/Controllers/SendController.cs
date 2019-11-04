0cc2f07cafee7c5bdebc1cdfc188e4ff66347d03
Jeppe <jeppe@bandsholm.com>
2019-11-04 12:36:19 +0100

    yeet

---
+++ Backend/Controllers/SendController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SendController : ControllerBase
+    {
+        // GET: api/Send/5
+        [HttpGet("{Serialized}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+    }
+}
