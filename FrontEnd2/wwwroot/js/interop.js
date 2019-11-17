1b90764e11d4fe4b08a377bbf1ba6762a50cb054
ms99uni <msamim18@student.aau.dk>
2019-11-17 13:09:00 +0100

    html api access to geolocation with js interop

---
+++ FrontEnd2/wwwroot/js/interop.js
@@ -0,0 +1,32 @@
+﻿function createAlert(locationString) {
+    alert(locationString);
+};
+
+var x = document.getElementById("demo");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    document.getElementById("demo").innerHTML = "Latitude: " + position.coords.latitude +
+        "<br>Longitude: " + position.coords.longitude;
+    alert("hej");
+}
+
+async function getCoordinates() {
+    if (navigator.geolocation) {
+        await navigator.geolocation.getCurrentPosition(UpdateUserLocation);
+    } else {
+        alert("Sorry, browser does not support geolocation!");
+    }
+}
+
+
+async function UpdateUserLocation(position) {
+    await DotNet.invokeMethodAsync("FrontEnd2", "UpdateUserLocation", position.coords.latitude, position.coords.longitude);
+}
