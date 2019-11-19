e8b84d189c9add676a477ec6cfac47953b756f28
KuskIV <mkusk18@student.aau.dk>
2019-11-19 09:53:47 +0100

    AlmostFIxed

--- B3-BiksBudget/Program.cs
+++ B3-BiksBudget/Program.cs
@@ -13,7 +13,7 @@ namespace BBGatherer
         {
             DataHandling dh = new DataHandling();
             //dh.GenerateDatabase();
-            dh.GenerateData();
+            dh.GenerateData(true, true);
             dh.TestCollection();
         }
     }
@@ -36,102 +36,101 @@ namespace BBGatherer
 
             List<CoopProduct> coopProducts = tryCoop.CoopFindEverythingInStore("24073");
 
+            //oopDoStuff tryCoop = new CoopDoStuff("d0b9a5266a2749cda99d4468319b6d9f");
+
             int count = 0;
-            foreach (CoopProduct c in coopProducts)
-            {
-                CoopDoStuff tryCoop = new CoopDoStuff("d0b9a5266a2749cda99d4468319b6d9f");
+                coopProducts = tryCoop.CoopFindEverythingInStore("2096");
 
-            coopProducts = tryCoop.CoopFindEverythingInStore("2096");
-
-            count = 0;
-            foreach (CoopProduct c in coopProducts)
-            {
-                count++;
-                Console.WriteLine(count);
-                dbConnect.AddProduct(new Product("B" + c.Ean, c.Navn, c.Navn2, c.Pris, "", "SuperBrugsen"));
-            }
+                count = 0;
+                foreach (CoopProduct c in coopProducts)
+                {
+                    count++;
+                    Console.WriteLine(count);
+                    dbConnect.AddProduct(new Product("B" + c.Ean, c.Navn, c.Navn2, c.Pris, "", "SuperBrugsen"));
+                }
 
 
-            /*RecipeCrawl WebRunner = new RecipeCrawl();
-            _ = WebRunner.GetRecipes(100, 1200, dbConnect);
+                /*RecipeCrawl WebRunner = new RecipeCrawl();
+                _ = WebRunner.GetRecipes(100, 1200, dbConnect);
 
-            Console.WriteLine("web runner begins... fear its power");
-            Console.ReadLine();*/
+                Console.WriteLine("web runner begins... fear its power");
+                Console.ReadLine();*/
         }
 
         public void TestCollection()
         {
-            dbConnect.AddUser("Test6", "Test", "BB");
+        dbConnect.AddUser("Test6", "Test", "BB");
 
-            /*List<Product> testList = new List<Product>();
+        /*List<Product> testList = new List<Product>();
 
-            Product tProd1 = new Product("F2141400000004", 5, "Full");
+        Product tProd1 = new Product("F2141400000004", 5, "Full");
 
-            Product tProd2 = new Product("F2141640000000", 5, "Full");
-            Product tProd3 = new Product("F4001724019831", 5, "Full");
+        Product tProd2 = new Product("F2141640000000", 5, "Full");
+        Product tProd3 = new Product("F4001724019831", 5, "Full");
 
-            testList.Add(tProd1);
-            testList.Add(tProd2);
-            testList.Add(tProd3);
+        testList.Add(tProd1);
+        testList.Add(tProd2);
+        testList.Add(tProd3);
 
-            dbConnect.AddListToStorage("Test6", testList);
+        dbConnect.AddListToStorage("Test6", testList);
 
-            testList.Remove(tProd2);
+        testList.Remove(tProd2);
 
-            dbConnect.UpdateStorage("Test6", testList);*/
+        dbConnect.UpdateStorage("Test6", testList);*/
 
-            List<Recipe> res = dbConnect.GetRecipes("Lam");
+        List<Recipe> res = dbConnect.GetRecipes("Lam");
 
-            foreach(Recipe r in res)
-            {
-                Console.WriteLine(r._ingredientList.Count);
-            }
+        foreach (Recipe r in res)
+        {
+            Console.WriteLine(r._ingredientList.Count);
+        }
 
-            /*
-            List <Recipe> recipes = new List<Recipe>();
-            Console.WriteLine("?????");
+        /*
+        List <Recipe> recipes = new List<Recipe>();
+        Console.WriteLine("?????");
 
-            foreach(Recipe r in res)
-            {
-                Console.WriteLine(r._Name + " AND " + r._ingredientList.Count);
-            }
-
-
-
-            /*public void TestCollection()
-            {
+        foreach(Recipe r in res)
+        {
+            Console.WriteLine(r._Name + " AND " + r._ingredientList.Count);
+        }
 
 
-                //Console.WriteLine(dbConnect.checkIfSomethingExist("users", "username", "Test"));
 
-                dbConnect.AddUser("Test6", "Test", "email");
-                //Console.WriteLine(dbConnect.CheckUser("Test", "Test"));
+        /*public void TestCollection()
+        {
+
 
             //Console.WriteLine(dbConnect.checkIfSomethingExist("users", "username", "Test"));
-            //dbConnect.AddUser("Test6", "Test", "email");
+
+            dbConnect.AddUser("Test6", "Test", "email");
             //Console.WriteLine(dbConnect.CheckUser("Test", "Test"));
+
+        //Console.WriteLine(dbConnect.checkIfSomethingExist("users", "username", "Test"));
+        //dbConnect.AddUser("Test6", "Test", "email");
+        //Console.WriteLine(dbConnect.CheckUser("Test", "Test"));
+
+        //List<Product> testList = new List<Product>();
+
+        //Product tProd1 = new Product("B2000020000002", 5, "Full");
+        //Product tProd2 = new Product("B2000110000004", 5, "Full");
+        //Product tProd3 = new Product("B2000060000000", 5, "Full");
+
+        //testList.Add(tProd1);
+        //testList.Add(tProd2);
+        //testList.Add(tProd3);
+        //dbConnect.AddListToStorage("Test6", testList);
+
 
             //List<Product> testList = new List<Product>();
 
-            //Product tProd1 = new Product("B2000020000002", 5, "Full");
-            //Product tProd2 = new Product("B2000110000004", 5, "Full");
-            //Product tProd3 = new Product("B2000060000000", 5, "Full");
+            //testList = dbConnect.GetStorageFromUsername("Test3");
 
-            //testList.Add(tProd1);
-            //testList.Add(tProd2);
-            //testList.Add(tProd3);
-            //dbConnect.AddListToStorage("Test6", testList);
-
-
-                //List<Product> testList = new List<Product>();
-
-                //testList = dbConnect.GetStorageFromUsername("Test3");
-
-                //foreach (Product p in testList)
-                //{
-                //    Console.WriteLine(p._timeAdded);
-                //}
-            }*/
+            //foreach (Product p in testList)
+            //{
+            //    Console.WriteLine(p._timeAdded);
+            //}
+        }*/
+    }
         }
     }
-}
+
