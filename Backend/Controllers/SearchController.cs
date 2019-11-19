080dfd36ecb30ce4ea66ea59a86e5449885cc8e9
Jeppe <jeppe@bandsholm.com>
2019-11-19 10:31:14 +0100

    simple getter isolated

--- Backend/Controllers/SearchController.cs
+++ Backend/Controllers/SearchController.cs
@@ -11,36 +11,12 @@ namespace Backend.Controllers
     [ApiController]
     public class SearchController : ControllerBase
     {
-        // GET: api/Search
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
         // GET: api/Search/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
+        [HttpGet("{searchterm}")]
+        public string Get(string searchterm)
         {
             return "value";
         }
 
-        // POST: api/Search
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT: api/Search/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
