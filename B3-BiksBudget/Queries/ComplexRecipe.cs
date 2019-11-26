1b066830da243c9d7c384c184fd9558353880c7b
ms99uni <msamim18@student.aau.dk>
2019-11-26 13:07:48 +0100

    working now

--- B3-BiksBudget/Queries/ComplexRecipe.cs
+++ B3-BiksBudget/Queries/ComplexRecipe.cs
@@ -5,9 +5,13 @@ using BBGatherer.Queries;
 
 namespace B3_BiksBudget.BBGatherer.Queries
 {
+    //Complex Recipe inherits the standard recipe, but extends it with an member/field of ComplexRecipeComponent, 
+    //which can be seen in the file ComplexRecipeComponent.cs
     public class ComplexRecipe : BBCollection.BBObjects.Recipe
     {
         public ComplexRecipeComponent _complexRecipeComponent = new ComplexRecipeComponent();
+
+        //Calls the the parent class' constructor, and initializes the _complexRecipeComponent
         public ComplexRecipe(int recipeID, string name, string description, List<BBCollection.BBObjects.Ingredient> ingredientList, float PerPerson, ComplexRecipeComponent complexRecipeComponent) : 
                         base(recipeID, name, description, ingredientList, PerPerson)
         {
