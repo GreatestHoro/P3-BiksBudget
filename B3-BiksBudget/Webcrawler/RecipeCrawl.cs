a69f314154aabd3b0580f8882787519b9da0c9ed
Jeppe <jeppe@bandsholm.com>
2019-11-28 14:56:09 +0100

    comments

--- B3-BiksBudget/Webcrawler/RecipeCrawl.cs
+++ B3-BiksBudget/Webcrawler/RecipeCrawl.cs
@@ -19,6 +19,15 @@ namespace BBGatherer.Webcrawler
     class RecipeCrawl
     {
         #region Webcrawler
+        /// <summary>
+        /// A webcrawler specefictly made to crawl the web site https://www.dk-kogebogen.dk/, i findes the diffrent elements of a recepie page,
+        /// such as name of the recipie, ingredients and the desribtion of the dish. 
+        /// It will also do some prossecing on each found element to clean them up and make them easier to workd with.
+        /// </summary>
+        /// <param name="start_page">The starting recepie on the website</param>
+        /// <param name="Last_page">The last recepie on the web site it should check</param>
+        /// <param name="dbConnect">The database that should get the finished recipies loaded into</param>
+        /// <returns></returns>
         public async Task GetRecipes(int start_page, int Last_page, DatabaseConnect dbConnect)
         {
             AssistingClasses functionality = new AssistingClasses();
@@ -76,10 +85,8 @@ namespace BBGatherer.Webcrawler
                         }
                         else
                         {
-
                             Console.WriteLine("Recipie determined false..");
                         }
-
                     }
                     else
                     {
@@ -92,8 +99,17 @@ namespace BBGatherer.Webcrawler
         }
         #endregion
 
-        #region ingrdient Creation
-        private Ingredient CreateIngriedient(String ind,out bool fatalError, DatabaseConnect dbConnect, AssistingClasses functionality)
+        #region ingredient Creation
+        /// <summary>
+        /// This method Creates and instans of the ingredient object, 
+        /// it calls method from diffrent classe to attempts to intrepret and edit the names to become more clear.
+        /// </summary>
+        /// <param name="ind">The raw ingrdient string with no editing or crossrefresing done</param>
+        /// <param name="fatalError">A bool which is triggered if the ingredient is determined uninterpretable</param>
+        /// <param name="dbConnect">Instace of the database used to save and edit the database</param>
+        /// <param name="functionality">A instance of a classe which contains many of the nessesary funtionalitys for the method</param>
+        /// <returns>Return a new instance</returns>
+        private Ingredient CreateIngriedient(string ind,out bool fatalError, DatabaseConnect dbConnect, AssistingClasses functionality)
         {
             float amount = functionality.getDetermin().DeterminAmount(ind);
             String unit = functionality.getDetermin().DeterminUnit(ind);
@@ -104,6 +120,14 @@ namespace BBGatherer.Webcrawler
             return new Ingredient(name.Trim(), unit, amount);
         }
 
+        /// <summary>
+        /// Method containnig the funtionality that evalute and edite the ingredient name.
+        /// </summary>
+        /// <param name="name">The raw name</param>
+        /// <param name="fatalError">A bool which is triggered if the ingredient is determined uninterpretable</param>
+        /// <param name="dbConnect">Instace of the database used to save and edit the database</param>
+        /// <param name="functionality">A instance of a classe which contains many of the nessesary funtionalitys for the method</param>
+        /// <returns>Returns a string that will be used as the ingrediient name</returns>
         private string nameEditing_Evalution(string name,out bool fatalError,DatabaseConnect dbConnect, AssistingClasses functionality) 
         {
             if (!String.IsNullOrWhiteSpace(name.Trim()))
@@ -129,6 +153,11 @@ namespace BBGatherer.Webcrawler
         #endregion
 
         #region(Process critical checks)
+        /// <summary>
+        /// Determines if the finished name is valid.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns>Gives a true when the name is invalid</returns>
         private bool EvaluateName(String name)
         {
             String[] SplitString = name.Split(" ");
@@ -164,6 +193,13 @@ namespace BBGatherer.Webcrawler
             return fatalError;
         }
 
+        /// <summary>
+        /// This method checks if a connection has been established and that the correct elements are present inside it
+        /// </summary>
+        /// <param name="name">raw html data from the section that contains the name of the recipie</param>
+        /// <param name="beskrivels">raw html data from the section that contains the desripbtion of the recipie</param>
+        /// <param name="ingredienser">raw html data from the section that contains the ingredients of the recipie</param>
+        /// <returns>False means that the webcrawler could not find the nessesary elements on the page</returns>
         private bool CheckIfPageFound(HtmlNodeCollection name, HtmlNodeCollection beskrivels, HtmlNodeCollection ingredienser)
         {
             if (name == null || beskrivels == null || ingredienser == null)
