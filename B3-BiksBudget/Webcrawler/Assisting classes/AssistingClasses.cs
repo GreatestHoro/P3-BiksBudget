b47646ea737b0d5e74556f562f1d541059e64b7a
Jeppe <jeppe@bandsholm.com>
2019-11-28 13:21:56 +0100

    Abstrction going trough the fucking roof

---
+++ B3-BiksBudget/Webcrawler/Assisting classes/AssistingClasses.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace B3_BiksBudget.Webcrawler.Assisting_classes
+{
+    class AssistingClasses
+    {
+        CrossRefrenceFuntionality _refs = new CrossRefrenceFuntionality();
+        Ingrdient_Determiner _Detminer = new Ingrdient_Determiner();
+        StringCleanUpFuntionality _clean = new StringCleanUpFuntionality();
+
+        public CrossRefrenceFuntionality getRefs() 
+        {
+            return _refs;
+        }
+        public Ingrdient_Determiner getDetermin() 
+        {
+            return _Detminer;
+        }
+        public StringCleanUpFuntionality getCleanFunc() 
+        {
+            return _clean;
+        }
+    }
+}
