fd866b3445dca14fe5c26c9e4e04baa0a55da571
Bbaltzersen <Bbaltz18@student.aau.dk>
2019-11-28 18:29:50 +0100

    Update UserHandling.cs

--- BBDatabase/DBHandling/UserHandling.cs
+++ BBDatabase/DBHandling/UserHandling.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Runtime.ExceptionServices;
 using System.Text;
 using BBCollection.DBConncetion;
 using Microsoft.AspNet.Identity;
@@ -10,16 +11,40 @@ namespace BBCollection.DBHandling
 {
     public class UserHandling
     {
+        /// <summary>
+        /// The add method uses the InsertUserToDB method to add a user from the parameters
+        /// username and password.
+        /// </summary>
+        /// <param name="username"></param> Username is a string containing the user's name.
+        /// <param name="password"></param> The password gets parsed as a string as well, and contains
+        /// the raw password that will be hashed and salted, later in the process.
         public void Add(string username, string password)
         {
             InsertUserToDB(username, password);
         }
 
+        /// <summary>
+        /// The Verify method takes a username and password parameter, and first takes the username
+        /// through the GetHashedPWFromUsername method, to get the correct password from the database,
+        /// and then it checks if the password's match eachother by using the checkHashedMethod.
+        /// </summary>
+        /// <param name="username"></param> A string of the username, that the user inserts in the login.
+        /// <param name="password"></param> A string of the password, that the user inserts in the login.
+        /// <returns></returns>
         public bool Verify(string username, string password)
         {
             return CheckHashedPassword(password, GetHashedPWFromUsername(username));
         }
 
+        /// <summary>
+        /// In the InsertUserToDB method, we have a INSERT query, that specifies the two collumns `username`
+        /// and `password` collumns, after that the values that have to be inserted into the collumns are specified,
+        /// this is done by using @'s to pinpoint in what spots the parameters have to be placed.
+        /// After that the string is saved in the MySqlCommand. This is followed by adding the parameters that 
+        /// have to be added to the string.
+        /// </summary>
+        /// <param name="username"></param> The username parameter is the username that the user inserted in the register page.
+        /// <param name="password"></param> The password parameter, is the hashed version of the password that the user inserted in the register page.
         private void InsertUserToDB(string username, string password)
         {
             string insertUserQuery =
@@ -32,15 +57,30 @@ namespace BBCollection.DBHandling
             new SQLConnect().NonQueryMSC(msc);
         }
 
+        /// <summary>
+        /// GetHashedPWFromUsername uses a SELECT query to get the password from the database.
+        /// </summary>
+        /// <param name="username"></param> The username parameter is the username string the user inserted in the login view.
+        /// <returns></returns>
         private string GetHashedPWFromUsername(string username)
         {
-            string password = null;
+            // First the password gets initiated with null, to have something to store the password 
+            // from the database in. Then the select query gets made, that will select the password
+            // From the `users` collumn in the database, where the username is equal to the String username.
+            string password = null; 
             string getPasswordQuery =
                 "SELECT password FROM users WHERE username = @Username ;";
 
+            // After the variables are made, we store the query in the MySqlCommand, and then specify the string
+            // That have to by inserted in replacement to the palceholder of the parameter, this is specified 
+            // as @Username
             MySqlCommand msc = new MySqlCommand(getPasswordQuery);
             msc.Parameters.AddWithValue("@Username", username);
 
+            // We then use the DynamicSimpleListSQL method, inside the SQLConnect() Class, that uses the query to
+            // create and return a Dataset. We store this dataset in DS and then we add the first element in the
+            // dataset in our password string and return it. We can do this because the username is unique, so the amount
+            // of rows that get returned, will always be 1 or 0.
             DataSet ds = new SQLConnect().DynamicSimpleListSQL(msc);
 
             if (ds.Tables[0].Rows.Count != 0)
@@ -51,6 +91,14 @@ namespace BBCollection.DBHandling
             return password;
         }
 
+        /// <summary>
+        /// CheckHashedpasword uses the class PasswordHasher with the method VerifyHashedPassword, 
+        /// included in the Identity class from Microsoft, to see if the password 
+        /// from the database is equal to the password that have been inserted in the login.
+        /// </summary>
+        /// <param name="password"></param> the password parameter, is the string the the user inserted in the login page.
+        /// <param name="hashedPassword"></param> the hashedpassword parameter, is the password that was retrieved from the database.
+        /// <returns></returns>
         private bool CheckHashedPassword(string password, string hashedPassword)
         {
             if (new PasswordHasher().VerifyHashedPassword(hashedPassword, password) == PasswordVerificationResult.Success)
@@ -63,6 +111,12 @@ namespace BBCollection.DBHandling
             }
         }
 
+        /// <summary>
+        /// ConvertPasswordToHash, is a method that takes use of microsoft's PasswordHasher to Hash and Salt the password before
+        /// It's stored in the database.
+        /// </summary>
+        /// <param name="password"></param> The password that the user inserted in the register page.
+        /// <returns></returns>
         private string ConvertPasswordToHash(string password)
         {
             return new PasswordHasher().HashPassword(password);
