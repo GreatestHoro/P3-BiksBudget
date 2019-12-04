ffb02b0b60c4fcddfee9737861bcebd35e8aa944
Roni Horo <rhoro18@student.aau.dk>
2019-12-04 17:31:28 +0100

    added autocomplesearch class

---
+++ FrontEnd2/Areas/Data/AutocompleteSearch.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontEnd2.Areas.Data
+{
+    public class AutocompleteSearch
+    {
+        public int valueField { get; set; }
+        public string textField { get; set; }
+    }
+}
